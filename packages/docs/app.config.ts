818ddfe2bf4fd0408231078addf98d454c756a46
linbudu <linbudu599@gmail.com>
2023-01-02 20:04:55 +0800

    feat: initialize docs

---
+++ packages/docs/app.config.ts
@@ -0,0 +1,28 @@
+export default defineAppConfig({
+  docus: {
+    title: "MustardCLI",
+    description: "IoC & Native ES Decotator based command-line app builder.",
+    url: "https://github.com/LinbuduLab/Mustard",
+    image: "/social-card-preview.png",
+    socials: {
+      github: "LinbuduLab",
+    },
+    github: {
+      root: "content",
+      edit: true,
+      contributors: true,
+    },
+    header: {
+      title: "LinbuduLab Projects",
+      logo: false,
+      showLinkIcon: true,
+    },
+    footer: {
+      credits: {
+        icon: "IconDocus",
+        text: "Powered by Docus",
+        href: "https://docus.com",
+      },
+    },
+  },
+});
---
+++ packages/docs/content/index.md
@@ -0,0 +1,23 @@
+---
+title: MustardCLI
+navigation: false
+layout: page
+---
+
+::block-hero
+---
+cta:
+  - Get Started →
+  - /intro
+secondary:
+  - View GitHub →
+  - https://github.com/LinbuduLab/Mustard
+snippet: npm installl typescript@^5.0.0
+---
+
+#title
+Mustard CLI
+
+#description
+IoC & Native ES Decotator based command-line app builder.
+::
