8074deead66b24006fa1f0b6640140b4cb449811
linbudu599 <linbudu599@gmail.com>
2023-01-23 18:07:19 +0800

    feat: init

--- packages/mustard-cli/source/Decorators/Input.ts
+++ packages/mustard-cli/source/Decorators/Input.ts
@@ -2,6 +2,11 @@ import type { AnyClassFieldDecoratorReturnType } from "../Typings/Temp";
 
 export class InputDecorator {
   /**
+   * TODO:
+   *
+   * @Input(name, alias, description)
+   * @Input({ name, alias, description })
+   *
    * Inject inputs after commands
    * @example
    * class RunCommand {
