7a0bfb0f5937026b9c0c35ce72dfc0c0779ca34c
linbudu599 <linbudu599@gmail.com>
2023-01-27 14:16:20 +0800

    chore: enhancement

--- packages/create-mustard-app/source/index.ts
+++ packages/create-mustard-app/source/index.ts
@@ -1,6 +1,13 @@
+import { createRequire } from "module";
+import fs from "fs-extra";
+import path from "path";
+import logSymbols from "log-symbols";
+
 import { MustardFactory, MustardUtils } from "mustard-cli";
 import { RootCommand, Option, App, Utils, Input } from "mustard-cli/decorator";
 import { CommandStruct, MustardApp } from "mustard-cli/cli";
+
+const require = createRequire(import.meta.url); // construct the require method
 
 type Template = "simple" | "complete";
 
@@ -13,15 +20,42 @@ class RootCommandHandle implements CommandStruct {
   public template: Template = "simple";
 
   @Input("directory to create the project in")
-  public dir: string = "./";
+  public dir: string = "./mustard-app";
 
   @Utils()
   public utils!: MustardUtils;
 
   public run(): void {
-    console.log(this.utils.colors.green("Hello World"));
-    console.log(this.dry, this.template);
-    console.log(this.dir);
+    console.log(
+      "create-mustard-app",
+      this.utils.colors.white(`v ${require("../package.json").version}\n`)
+    );
+
+    const createDir = path.resolve(this.dir);
+
+    console.log(`Creating project in ${this.utils.colors.white(createDir)}...`);
+
+    const template = ["simple", "complete"].includes(this.template)
+      ? this.template
+      : "simple";
+
+    console.log(`Using template ${this.utils.colors.white(template)}...`);
+
+    const templatePath = path.resolve(`./template-${template}`);
+
+    try {
+      fs.ensureDirSync(createDir);
+      fs.copySync(templatePath, createDir, {});
+
+      console.log("\n", logSymbols.success, "Project created successfully!");
+    } catch (error) {
+      fs.removeSync(createDir);
+      console.log(
+        logSymbols.error,
+        "Project creation failed wtih following error:"
+      );
+      throw error;
+    }
   }
 }
 
--- packages/mustard-cli/source/Typings/Temp.ts
+++ packages/mustard-cli/source/Typings/Temp.ts
@@ -1,9 +1,24 @@
-export type AnyClassFieldDecoratorReturnType = (
-  a1: undefined,
-  context: ClassFieldDecoratorContext
-) => (initialValue: unknown) => unknown | void;
-
 export type AnyClassDecoratorReturnType = (
   target: any,
   context: ClassDecoratorContext
 ) => void;
+
+export type AnyClassMethodDecoratorReturnType = (
+  self: Function,
+  context: ClassMemberDecoratorContext
+) => Function | void;
+
+export type AnyClassGetterDecoratorReturnType = (
+  self: Function,
+  context: ClassMemberDecoratorContext
+) => Function | void;
+
+export type AnyClassSetterDecoratorReturnType = (
+  self: Function,
+  context: ClassMemberDecoratorContext
+) => Function | void;
+
+export type AnyClassFieldDecoratorReturnType = (
+  a1: undefined,
+  context: ClassFieldDecoratorContext
+) => (initialValue: any) => any | void;
